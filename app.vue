a2c57cdc967892d3e242066d228a2bbd05620190
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-08 15:49:25 +0100

    end

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <NuxtPage/>
+    <img src="/assets/Baki-Logo.png">
     <NuxtLink to="/rebonjour">
   </NuxtLink>
   <NuxtLink to="/bonjour">
   </NuxtLink>
   </div>
 </template>
+
+<script>
+import baseCss from './assets/base.css'
+</script>
 
--- pages/bonjour.vue
+++ pages/bonjour.vue
@@ -2,11 +2,4 @@
 <h1>
     HelloWorld
 </h1>
-<img src="/assets/Baki-Logo.png">
 </template>
-
-<style scoped>
-  h1{
-    color: aquamarine;
-  }
-</style>
